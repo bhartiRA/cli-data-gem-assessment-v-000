004c8ff1d2d54575d7218e59ba2df6020123d84f
bharti gurnaney <bharti0717@yahoo.com>
2016-11-04 01:45:02 +0000

    Done.

--- lib/Best_xbox_games/scraper.rb
+++ lib/Best_xbox_games/scraper.rb
@@ -1,16 +1,17 @@
 class Best_xbox_games::Scraper
 
   def get_page
-    Nokogiri::HTML(open("http://www.metacritic.com/feature/25-best-ios-games"))
+    Nokogiri::HTML(open("http://www.rediffmail.com"))
   end
 
   def scrape_game_index
-    self.get_page.css("table.listtable")
+    self.get_page.css("div.newimgthumb a")
   end
 
   def make_games
-    scrape_games_index.each do |r|
-      2016_best_xbox_games::Games.new_from_index_page(r)
+    scrape_game_index.each do |r|
+    Best_xbox_games::Games.new_from_index_page(r)
     end
+    binding.pry
   end
 end
