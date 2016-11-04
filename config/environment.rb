49e9219c6098ffd7296548c61ff61fc02c391e42
bharti gurnaney <bharti0717@yahoo.com>
2016-11-04 00:22:48 +0000

    Done.

---
+++ config/environment.rb
@@ -0,0 +1,7 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+require_relative '../lib/Best_xbox_games/scraper'
+require_relative '../lib/Best_xbox_games/games'
+require_relative '../lib/Best_xbox_games/cli'
